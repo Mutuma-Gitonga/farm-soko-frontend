92e27e602ce5cbfaf066bf9418c97e9f54328dc1
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-10 18:35:25 +0300

    fix: redo to allow only farmer login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,15 @@
 import React, { useState } from "react";
 
 function Login(props) {
-  // { setSignUpLoginAsConsumer, signUpLoginAsConsumer, setFarmer || setConsumer }
-
-  const [consumerUsername, setConsumerUsername] = useState("");
-  const [consumerPassword, setConsumerPassword] = useState("");
 
   const [farmerUsername, setFarmerUsername] = useState("");
   const [farmerPassword, setFarmerPassword] = useState("");
+  const [loginErrors, setLoginErrors] = useState([])
 
-  function handleConsumerLoginSubmit(e) {
-    e.preventDefault();
-    fetch("/consumer_login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ 
-        username: consumerUsername, 
-        password: consumerPassword 
-      }),
-    }).then((r) => {
-      if (r.ok) {
-        r.json().then((consumer) => props.setConsumer(consumer));
-      }
-    });
-  }
 
   function handleFarmerLoginSubmit(e) {
     e.preventDefault();
-    fetch("/farmer_login", {
+    fetch("https://farm-soko-api-production.up.railway.app/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -40,41 +20,15 @@ function Login(props) {
       }),
     }).then((r) => {
       if (r.ok) {
-        r.json().then((consumer) => props.setFarmer(consumer));
+        r.json().then((farmer) => props.setFarmer(farmer));
+      } else {
+        r.json().then((errorData) => setLoginErrors(errorData.errors))
       }
     });
   }
 
-  const handleLoginToggleClick = () => {
-    props.setSignUpLoginAsConsumer((signUpLoginAsConsumer) => !signUpLoginAsConsumer);
-  }
-
   return (
     <div>
-      {props.signUpLoginAsConsumer ? (
-        <form onSubmit={handleConsumerLoginSubmit}>
-          <h1>Consumer Login Form</h1>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            autoComplete="off"
-            value={consumerUsername}
-            onChange={(e) => setConsumerUsername(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={consumerPassword}
-            onChange={(e) => setConsumerPassword(e.target.value)}
-          />
-          <button type="submit">Login</button>
-          <br/>
-          <span style={{fontWeight: "bold", color: "red"}}>Not a Consumer?</span>
-          <button style={{fontWeight: "bold", color: "blue"}} onClick={handleLoginToggleClick}>Click Here for Farmer Login Form</button>
-        </form>) : (
         <form onSubmit={handleFarmerLoginSubmit}>
           <h1>Farmer Login Form</h1>
           <label htmlFor="username">Username</label>
@@ -93,12 +47,15 @@ function Login(props) {
             value={farmerPassword}
             onChange={(e) => setFarmerPassword(e.target.value)}
           />
+          {loginErrors.length > 0 && (
+            <ul style={{ color: "red" }}>
+              {loginErrors.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          )}
           <button type="submit">Login</button>
-          <br/>
-          <span style={{fontWeight: "bold", color: "red"}}>Not a Farmer?</span>
-          <button style={{fontWeight: "bold", color: "blue"}} onClick={handleLoginToggleClick}>Click Here for Consumer Login Form</button>
         </form>
-      )}
     </div>
   );
 }
