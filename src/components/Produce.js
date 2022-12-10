ba0c585b56739781d0b016e478e89520d41282e2
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-10 18:43:56 +0300

    fix: conditionally render rows for logged in farmer

--- src/components/Produce.js
+++ src/components/Produce.js
@@ -1,14 +1,26 @@
 import React from "react";
 
-function Produce({produce}) {
-  return (
-    <tr>
-      <td>{produce.name}</td>
-      <td>{produce.quantity}</td>
-      <td>{produce.units}</td>
-      <td>{produce.unit_price}</td>
-    </tr>
-  );
+function Produce({produce, farmerObj}) {
+  
+
+
+    if (farmerObj.id === produce.farmer.id) {
+      return (
+         <tr>
+          <td style={{border: "2px solid black"}}>{produce.id}</td>
+          <td style={{border: "2px solid black"}}>{produce.name}</td>
+          <td style={{border: "2px solid black"}}>{produce.quantity}</td>
+          <td style={{border: "2px solid black"}}>{produce.units}</td>
+          <td style={{border: "2px solid black"}}>{produce.unit_price}</td>
+          <td style={{border: "2px solid black"}}>{produce.farmer.name}</td>
+          <td style={{border: "2px solid black"}}>{produce.farmer.town}</td>
+          <td style={{border: "2px solid black"}}>{produce.farmer.phone}</td>
+        </tr>
+      );
+    } else {
+      return null;
+    }
+
 }
 
 export default Produce;
