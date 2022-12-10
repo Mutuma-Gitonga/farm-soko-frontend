8e3ad77791ac61ccb6cba2fdbf3355dbdd4fedb1
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-10 07:22:13 +0300

    feat: add to fetch farmer produce

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,49 @@
 function Home(props) {
   if (props.farmer) {
+    const [producesList, setProducesList] = useState([]);
+    const [updatedProducesList, setUpdatedProducesList] = useState([]);
+
+    // const [searchChar, setSearchChar] = useState("");
+
+    useEffect(() => {
+      fetch("https://farm-soko-api-production.up.railway.app/produce")
+        .then((res) => res.json())
+          .then(listData => {
+            setProducesList(listData)
+            setUpdatedProducesList(listData)
+            // console.log(listData)
+          });
+    },[]);
+
+    useEffect(() => {
+      setUpdatedProducesList(producesList);
+    },[producesList]);
+
+    function addNewProduce(newTransactionObj) {
+      const updatedTransactions = [...producesList, newTransactionObj];
+      setProducesList(updatedTransactions);
+    }
+
+    function searchProduce(newSearchString) {
+      const newFilteredList = producesList.filter(transaction => {
+        if(newSearchString === "") {
+          return true;
+        } 
+        return transaction.description.toLowerCase().split(" ").join("").includes(newSearchString)
+      });
+
+      setUpdatedProducesList(newFilteredList);
+    }
+
     return (
-      <h1>Welcome, farmer!</h1>
+
+      <div className="ui raised segment">
+        <div className="ui segment violet inverted">
+          <h2>Welcome, farmer!</h2>
+        </div>
+        <ProduceContainer searchProduce={searchProduce} producesList={updatedProducesList} addNewProduce={addNewProduce} />
+      </div>
+      
     );
   } else if (props.consumer) {
     return (
