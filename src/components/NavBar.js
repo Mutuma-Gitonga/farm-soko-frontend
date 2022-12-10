a24548bf50286254022a6bb87be6bff3917e7a9e
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-10 18:33:50 +0300

    style: web title & access buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function NavBar(props) {
-  // { farmerConsumerLoggedInStatus, setFarmer || setConsumer, signUpAsConsumer }
 
   function handleLogoutClick() {
     
-    if(props.setFarmer){
-      fetch("/farmer_logout", {method: "DELETE"})
+    fetch("https://farm-soko-api-production.up.railway.app/logout", {method: "DELETE"})
       .then((res) => {
         if(res.ok) {
           props.setFarmer(null);
         }
-      })
-    } else {
-      fetch("/consumer_logout", {method: "DELETE"})
-      .then((res) => {
-        if(res.ok) {
-          props.setConsumer(null);
-        }
-      })
-    }
+    });
     
   }
 
@@ -31,12 +21,12 @@ function NavBar(props) {
       </div>
       <div><h1 style={{fontWeight: "bold", color: "green"}}>FarmSoko&#8482;</h1><p style={{fontWeight: "bold", color: "mediumpurple", fontSize: "1.2em"}}>The Fresh Organic Food Market!</p></div>
       <div>
-        {props.farmerConsumerLoggedInStatus ? (
+        {props.farmer ? (
           <button onClick={handleLogoutClick}>Logout</button>
         ) : (
           <>
-            <Link to="/signup">SignUp</Link>
-            <Link to="/login">Login</Link>
+            <Link to="/signup">Farmer Signup</Link>
+            <Link to="/login">Farmer Login</Link>
           </>
         )}
       </div>
